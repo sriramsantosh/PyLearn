3e7415c829d37ff8fc112dcd735acd75305281d4
Sriram Santosh Aripirala <sriramsantosh_aripirala@intuit.com>
2018-10-27 10:41:19 -0700

    Add changes to test

--- test.py
+++ test.py
@@ -6,5 +6,7 @@ num2 = 6.3
 # Add two numbers
 sum = float(num1) + float(num2)
 
+print ('New changes, should be breaking, lets hope for that')
+print ('One more, just in case')
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
